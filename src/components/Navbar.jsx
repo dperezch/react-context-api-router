8f128c2e9150974622e8e3bde449a9fa90fcfe2b
dperezch <davarmando@gmail.com>
2023-03-18 12:17:16 -0300

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+
+import { NavLink} from "react-router-dom"
+import { useUserContext } from "../context/UserContext";
+
+const Navbar = () => { 
+
+    console.log(useUserContext());
+
+    const {user, setUser} = useUserContext()
+
+    return (
+        <nav>
+            <NavLink to="/">Home</NavLink> |
+            {
+                user && (
+                    <>
+                        <NavLink to="/dashboard">Dashboard</NavLink>
+                        <button onClick={()=> setUser(false)}>Logout</button>
+                    </>
+                    
+                )
+            }
+            
+        </nav>
+    )
+ }
+
+ export default Navbar
